616b22f3e16ebf7dcf7c50d14947605481af370c
annabosunova <130104186+annabosunova@users.noreply.github.com>
2024-03-21 14:15:12 -0400

    Update documentation

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
+/**
+ * A Node.js script that sets up a HTTP server
+ * It requires http and fs (file system) modules,
+ * that are used to create an HTTP server and read a file
+ */
+
 const http = require('http');
 const fs = require('fs');
 const port = 3000;
-
+// create server
 const server = http.createServer(function(req,res) {
+    // update response status
     res.writeHead(200, {'Content-Type':'text/html'})
+    // read index.html
     fs.readFile('index.html', function(error, data) {
         if(error) {
+            // update response status in case of error
             res.writeHead(404);
-            res.write('Error: File not found')
+            res.write('Error: File not found');
          } else {
             res.write(data);
          }
          res.end();
     })
 })
-
+// start server listening
 server.listen(port,function(error) {
     if(error) {
         console.log('Something went wrong', error);
